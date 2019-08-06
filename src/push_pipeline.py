7312c05b29f7ab9da7ac758ac0a1e2300b1a4d7c
Deepak Seeni <dseeni@gmail.com>
2019-08-05 18:33:21 -0700

    Slight tweaks

--- src/push_pipeline.py
+++ src/push_pipeline.py
@@ -4,6 +4,30 @@ from contextlib import contextmanager
 import csv
 from itertools import islice
 
+# TODO: Look at the pulling example and rewrite it all as a push pipeline
+# We can do this be making the reader yield and row and send it to parser that
+# sends it to broadcaster
+
+# TODO: Parse the file with correct types, via type sniffing function
+# TODO: Make it as generic as possible to handle N filters and N files and N
+# destination files
+# And N source files!!!
+
+# TODO: Solution must take in arbitrary amount of filters
+# Any 3 filters will result with 2 records on the screen only
+# if you did it all right
+
+# TODO: Implement averager for average length of lines in file
+# # TODO: Lookup the print_file_row() function in Fred's notes on GitHub.
+
+
+# headers = ('make', 'model', 'year', 'vin', 'color')
+
+# def get_dialect(file_obj):
+#     sample = file_obj.read(2000)
+#     dialect = csv.Sniffer().sniff(sample)
+#     file_obj.seek(0)
+#     return dialect
 
 def coroutine(fn):
     def inner(*args, **kwargs):
@@ -81,30 +105,6 @@ def pipeline_coro():
     while True:
         data_row = yield
         broadcaster.send(data_row)
-# TODO: Look at the pulling example and rewrite it all as a push pipeline
-# We can do this be making the reader yield and row and send it to parser that
-# sends it to broadcaster
-
-# TODO: Parse the file with correct types, via type sniffing function
-# TODO: Make it as generic as possible to handle N filters and N files and N
-# destination files
-# And N source files!!!
-
-# TODO: Solution must take in arbitrary amount of filters
-# Any 3 filters will result with 2 records on the screen only
-# if you did it all right
-
-# TODO: Implement averager for average length of lines in file
-# # TODO: Lookup the print_file_row() function in Fred's notes on GitHub.
-
-
-# headers = ('make', 'model', 'year', 'vin', 'color')
-
-# def get_dialect(file_obj):
-#     sample = file_obj.read(2000)
-#     dialect = csv.Sniffer().sniff(sample)
-#     file_obj.seek(0)
-#     return dialect
 
 
 
